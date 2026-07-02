79a7fbc5522ea010c1628ba90e1c59bfddd6efdf
Muhammadsyahril20 <bang.syah201104@gmail.com>
2026-07-03 00:39:06 +0700

    Upgrade api/index.php anti crash Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
+
+// 1. Buat folder virtual di memori Vercel biar Laravel bebas nulis file
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
+// 2. Paksa Laravel pakai konfigurasi Serverless
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+putenv('APP_DEBUG=true');
+
+// 3. Panggil inti Laravel
 require __DIR__ . '/../public/index.php';
